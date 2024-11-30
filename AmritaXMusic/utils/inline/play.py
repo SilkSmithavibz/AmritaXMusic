d058d05c5cf845bc164353fafd41967cb8e593dd
Marwin2002 <musicbotid@gmail.com>
2024-12-01 00:05:14 +0530

    Update and rename play (1).py to play.py

--- AmritaXMusic/utils/inline/play.py
+++ AmritaXMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from Seistaxmusic.utils.formatters import time_to_seconds
+from AmritaXMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
