cf8d25134efa68646cb115bf2d4665ad00f708a5
SilkSmithavibz <Silksmithavibz@gmail.com>
2024-12-25 18:55:56 +0530

    Update couples.py

--- AmritaXMusic/plugins/tools/couples.py
+++ AmritaXMusic/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="|| ˹𖤍 𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗚𝗥𝗢𝗨𝗣𖤍˼ ||",
-            url=f"https://t.me/AmritaMusicBot?startgroup=true",
+            url=f"https://t.me/Ennavall_bot?startgroup=true",
         ),
     ],
 ]
