ca97029e051344c0408fa81bafc4ee173a9704c1
SilkSmithavibz <Silksmithavibz@gmail.com>
2024-12-29 19:17:48 +0530

    Update Youtube.py

--- AmritaXMusic/platforms/Youtube.py
+++ AmritaXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ANNIEMUSIC.utils.database import is_on_off
-from ANNIEMUSIC.utils.formatters import time_to_seconds
+from AmritaXMusic.utils.database import is_on_off
+from AmritaXMusic.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "ANNIEMUSIC/assets/cookies.txt"
+cookies_file = "AmritaXMusic/assets/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
@@ -310,7 +310,7 @@ class YouTubeAPI:
                     {
                         "key": "FFmpegExtractAudio",
                         "preferredcodec": "mp3",
-                        "preferredquality": "192",
+                        "preferredquality": "8000",
                     }
                 ],
                 "cookiefile": cookies_file,
